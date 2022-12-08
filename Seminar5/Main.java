464aad113a7b2fc2da9c167446e42d9f2e25fe4e
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-12-08 17:45:22 +0300

    Seminar 5

---
+++ Seminar5/Main.java
@@ -0,0 +1,25 @@
+package Seminar5;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Task1 taskOne = new Task1();
+        // Map<Integer, String> passports = taskOne.fillMap(new HashMap<>());
+        // Scanner in = new Scanner(System.in, "Cp866");
+        // taskOne.getPassportInfoByName(passports, in.next());
+
+        // Task2 taskTwo = new Task2();
+        // String add = "add";
+        // String ogg = "ogg";
+        // System.out.println(taskTwo.isIsomorphic(add, ogg));
+
+        Task3 taskThree = new Task3();
+        taskThree.sortedMap("Мороз и солнце день чудесный Еще ты дремлешь друг прелестный Пора красавица проснись");
+
+    }
+
+}
